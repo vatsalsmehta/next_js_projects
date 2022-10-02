4ac7a5fa6efe5c1d7c79cdcf96a1633c894c413b
vsmehta <vmehta@zmags.com>
2022-10-02 15:33:37 +0530

    ui enhancements

--- popups_mui/Components/Dialogcomponent.tsx
+++ popups_mui/Components/Dialogcomponent.tsx
@@ -24,7 +24,10 @@ const DialogComponent=(props: DialogTitleProps)=>{
         //open={isdialogopen} is the logic of opening and closing of of popup as popup will open when isdialogopen is true and close when isdialogopen is false
         //if we add onclose any click outside the popup will result in closing of popup
        <Dialog id={id} classes={{ paper: css.dialogBoxStyles }}open={isdialogopen} onClose={() => setisdialogopen(false)} >
-            <DialogTitle>Popup Opened</DialogTitle>
+            <Box className={css.dialogHeader}>
+                <DialogTitle className={css.dialogTitle}>Popup Opened</DialogTitle>
+            </Box>
+            
             <Box style={{ display: "flex", flexDirection: "column" }}>
                 {data.length!=0 && data.map((product,index)=>{  
                     return(
