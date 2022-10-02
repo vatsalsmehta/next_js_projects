0a7d187abb7d43131d30e86d3b564023112fb0dd
vsmehta <vmehta@zmags.com>
2022-10-02 15:31:57 +0530

    setup basic structure of autocomplete mui

---
+++ autocomplete_mui/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+class IndexHomepage extends React.Component{
+  render(): React.ReactNode {
+    return(
+      <h1>Hello</h1>
+    )
+  }
+}
+
+export default IndexHomepage;
