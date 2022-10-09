820fb6312f98e3bd3630f440af206bf9737e3a77
vsmehta <vatsalsmehta@gmail.com>
2022-10-09 21:09:26 +0530

    Setup SidebarNav

---
+++ sidebarnav_grid_mui/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class IndexPage extends React.Component{
+  render(): React.ReactNode {
+    return(
+      <>Hi
+      </>
+    )
+  }
+}
+
+export default IndexPage;
