c961adc394e8f79dba63553659bf9b1eda42d42e
vatsalsmehta <vatsalsmehta@gmail.com>
2022-11-14 14:59:55 +0530

    Initial commit for React Drag and Drop

---
+++ reactbeautifuldnd/pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+class IndexPage extends React.Component {
+  render(): ReactNode {
+    return(
+      <>Hello</>
+    )
+  }
+}
+export default IndexPage;
+
