906d1f4b5ec3a2a31f70bfc6fdc72c8eb5db7485
vatsalsmehta <vatsalsmehta@gmail.com>
2022-11-27 23:08:55 +0530

    Added Drag and Drop for Singular lists using react-beautiful dnd

--- reactbeautifuldnd/pages/singleList/index.tsx
+++ reactbeautifuldnd/pages/singleList/index.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import {Box} from '@mui/material';
+import { makeStyles } from '@material-ui/core';
 
+export interface pageData {
+    id: string;
+}
 
 const data = [
     {
@@ -23,35 +28,83 @@ const data = [
     }
 ]
 
+const useStyles = makeStyles(() => ({
+
+    listContainer: {
+        width: "700px",
+        height: "400px",
+        padding: "10px",
+        backgroundColor: "#ADD8E6",
+        // the below three will center content inside this to center like the list boxes
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "auto", // this will center the whole container to center of page
+        marginTop: "100px"
+    },
+    listItems: {
+        width: "130px",
+        height: "30px",
+        margin: "5px",
+        color: "#fff",
+        backgroundColor: "#DBA39A",
+        // the below three will center content inside list boxes to center like the text of c3_page
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+
+}));
 
 export const SingleListIndexPage = () => {
 
-    const [pages, setPages] = useState<any[]>();
-
+    const [pages, setPages] = useState<any[]>([]);
+    const classes = useStyles();
     useEffect(() => {
         setPages(data);
     }, [])
 
-    const dragEndHandler = (result:any) => {
-        console.log("drag Ends", result);
+    const dragEndHandler = (event:any) => {
+        if (!event.destination) {
+            console.log("returning");
+            return;
+        }
+
+        const _pageItems = [...pages];
+        // get the item details/content user dragged from the event , its there as source index
+        const draggedItemContent = _pageItems[event.source.index];
+        console.log("user tried to Drag: ", draggedItemContent);
+
+        //remove the item user dragged from array and we'll add it to our target destination
+        _pageItems.splice(event.source.index, 1);
+
+        // replace without removing anything
+        // add draggedItemContent to index where user finished drag and shift rest of everything ahead
+        _pageItems.splice(event.destination.index, 0, draggedItemContent);
+
+        console.log("After dropping items are: ", _pageItems);
+
+        setPages(_pageItems);
+        console.log("drag Ends");
     }
 
 
     return(
-        <>
+        <Box className={classes.listContainer}>
             <DragDropContext onDragEnd = {dragEndHandler}>
-                {/* We make the whole outer division as Droppable and elements inside them draggable */}
-                <Droppable droppableId= "vatsal">
+                {/* We make the whole outer division as Droppable and elements/listitems inside them draggable */}
+                <Droppable droppableId= "pages-reodering">
                     {
                         (provided, snapshot) => (
                             <div {...provided.droppableProps} ref={provided.innerRef}>
-                                {data.map((item, index) => {
+                                {pages.map((item, index) => {
                                     return(
                                         <Draggable key={item.id} draggableId={item.id} index = {index}>
                                             {(provided, snapshot) => (
-                                                <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                <Box ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} className={classes.listItems}>
                                                     {item.id}
-                                                </div>
+                                                </Box>
                                             )
                                             }
                                         </Draggable>
@@ -62,7 +115,7 @@ export const SingleListIndexPage = () => {
                     }
                 </Droppable>
             </DragDropContext>
-        </>
+        </Box>
     )
 };
 
